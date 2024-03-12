5daa22fc610b3dfde110522f11b0bb7d7527231a
raiks01 <raiks01@tlu.ee>
2024-03-12 10:30:35 -0400

    changes made from theia lab enviroment

--- src/components/CartValue.js
+++ src/components/CartValue.js
@@ -2,6 +2,16 @@ import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const CartValue = () => {
+    const { expenses, Location } = useContext(AppContext);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += (item.uniteprice * item.quantity));
+    }, 0);
+
+    return (
+        <div className='alert alert-primarty'>
+            <span>Cart Value: {Location}{totalExpenses}</span>
+        </div>
+    );
 };
 
 export default CartValue;
